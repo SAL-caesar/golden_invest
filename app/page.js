38bfcbe300431f6e92844224361a2aee189f79d8
SAL-caesar <a9452167@gmail.com>
2025-11-20 09:13:04 -0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,6 @@ export default function Home() {
   return (
     <div style={{textAlign:"center",marginTop:"50px"}}>
       <h1>مرحبا بك في Golden Invest</h1>
-      <p>الرجاء تسجيل الدخول للمتابعة</p>
       <a href="/auth/login">تسجيل الدخول</a>
     </div>
   );
